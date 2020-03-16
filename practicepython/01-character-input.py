28634127f880a04720f49b4b20e9e8decfb07b13
Rabindra Joshi <mail@rjoshi.net>
2020-03-16 09:37:36 +0545

    Add question 1: character input

--- practicepython/01-character-input.py
+++ practicepython/01-character-input.py
@@ -1,1 +1,11 @@
 # Question 1
+# https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+
+age = int(age)
+
+age_to_100 = 100 - age
+
+print(f"{name}, you'll turn 100 in {age_to_100} years")
